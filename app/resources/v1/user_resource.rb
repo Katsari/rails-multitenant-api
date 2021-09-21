c78c9b3976b824131de1f88611fef0ecb4992792
Maria Guarecuco <mgabriela.guarecuco@gmail.com>
2021-09-21 18:05:00 -0400

    fix: total count for user resource
    Temporary fix, hopefully

--- app/resources/v1/user_resource.rb
+++ app/resources/v1/user_resource.rb
@@ -14,4 +14,13 @@ class V1::UserResource < V1::APIResource
   belongs_to :role
 
   filter :role_id, :integer
+
+  # Overwrite total count because original query (scope.distinct.count(:all)) originates a StatementInvalid error
+  # Reason: distinct is applied to all columns and users table has a column of type json (tokens)
+  # PostgreSQL has no equality operator for json, so DISTINCT originates this error
+  stat :total do
+    count do |scope, attr|
+      scope.count(:all)
+    end
+  end
 end
